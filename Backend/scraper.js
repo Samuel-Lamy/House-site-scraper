ede2cedc965684f56194708ea05aeb174012c360
Samuel Lamy <samuel.lamy@umontreal.ca>
2024-01-24 20:54:11 -0500

    Select price range

--- Backend/scraper.js
+++ Backend/scraper.js
@@ -13,7 +13,51 @@ const scrapeWebsite = async () => {
     "https://www.centris.ca/fr/propriete~a-vendre~trois-rivieres"
   );
 
-  await page.waitForSelector("#property-result");
+  await page.waitForSelector("#didomi-notice-agree-button");
+  await page.click("#didomi-notice-agree-button");
+
+  await page.waitForSelector("#filter-search");
+  await page.click("#filter-search");
+
+  await page.waitForSelector("#PriceSection-secondary");
+  await page.screenshot({ path: "screenshot.png" });
+  const priceSection = await page.$("#PriceSection-heading-filters");
+  await priceSection.click("button");
+
+  await page.waitForSelector("#PriceSection-secondary");
+  const priceSectionSecondary = await page.$("#PriceSection-secondary");
+  await page.waitForSelector(".min-slider-handle");
+  const minSlider = await priceSectionSecondary.$(".min-slider-handle");
+  const maxSlider = await priceSectionSecondary.$(".max-slider-handle");
+  const posMinSlider = await minSlider.asElement().boundingBox();
+  const posMaxSlider = await maxSlider.asElement().boundingBox();
+  const percentageLowestBoundPrice = 0.21;
+  await page.mouse.click(
+    posMinSlider.x +
+      percentageLowestBoundPrice * (posMaxSlider.x - posMinSlider.x),
+    posMinSlider.y
+  );
+  let percentageHighestBoundPrice = 1;
+  const WantedPercentageHighestBoundPrice = 0.37;
+  while (percentageHighestBoundPrice !== WantedPercentageHighestBoundPrice) {
+    const nextHalf =
+      (percentageHighestBoundPrice - percentageLowestBoundPrice) / 2 +
+      percentageLowestBoundPrice +
+      0.02;
+    if (nextHalf < WantedPercentageHighestBoundPrice) {
+      percentageHighestBoundPrice = WantedPercentageHighestBoundPrice;
+    } else {
+      percentageHighestBoundPrice = nextHalf;
+    }
+    await page.mouse.click(
+      posMinSlider.x +
+        percentageHighestBoundPrice * (posMaxSlider.x - posMinSlider.x),
+      posMinSlider.y
+    );
+  }
+
+  await page.waitForSelector(".js-trigger-search");
+  await page.click(".js-trigger-search");
   await page.screenshot({ path: "screenshot.png" });
 
   const data = await page.evaluate(() => {
