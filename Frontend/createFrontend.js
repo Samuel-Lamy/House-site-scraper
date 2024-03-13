2a8934d62e7af0f5792558ed6805845eb8de08d0
Samuel Lamy <samuellamy1@gmail.com>
2024-03-12 20:52:45 -0400

    Parse the html to remove all default properties

--- Frontend/createFrontend.js
+++ Frontend/createFrontend.js
@@ -1,10 +1,18 @@
+import { JSDOM } from "jsdom";
+
 export const createFrontend = async () => {
   return await fetch(
     "https://www.centris.ca/fr/propriete~a-vendre~trois-rivieres"
   )
     .then((response) => response.text())
     .then((html) => {
-      return html;
+      const dom = new JSDOM(html);
+      const document = dom.window.document;
+      const propertiesToDelete = document.querySelectorAll(
+        ".property-thumbnail-item"
+      );
+      propertiesToDelete.forEach((property) => property.remove());
+      return dom.serialize();
     })
     .catch((error) => {
       console.error("Error fetching or modifying webpage:", error);
